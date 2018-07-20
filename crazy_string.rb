481b2178327fe60dc894537afeb4e2610bd0ba6c
Priya Kumar <priya.kumar08@yahoo.com>
2018-07-20 23:59:59 +0000

    Done.

--- crazy_string.rb
+++ crazy_string.rb
@@ -1,5 +1,8 @@
 # write your method here
 
-def crazy_strings(a,b)
-
+def crazy_strings(word_1,word_2)
+"#{word_1} #{word_2}.upcase.reverse"
+"#{word_1} #{word_2}.swapcase.gsub("s","z")"
 end
+
+crazy_strings("apple", "")
